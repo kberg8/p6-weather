77a974fbb7373d0bb0916ff56ca041b779d11e89
kberg8 <kberg@Kelseys-iMac.local>
2014-03-10 20:38:56 -0700

    3

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     location: 'Cheney, WA',
     woeid: '',
     unit: 'f',
+
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
@@ -16,6 +17,7 @@ $(document).ready(function(){
   
       $("#weather").html(html);
     },
+    
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
     }
