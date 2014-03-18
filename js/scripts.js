66fb999257d237e90039bfc548517f8313108410
kberg8 <kberg@Kelseys-iMac.local>
2014-03-17 21:19:16 -0700

    Working Weather App

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,55 @@
-// Make Foundation Go!
+// Run Foundation
 $(document).foundation();
 
-// Your Awesome Scripts!
+//Scripts
 
   $('button').on('click', function() {
   
-  // 1. Get & store entered zipcode
+
   var zipcode = $('#getWeather').val();
   
-  // 2. Pass weather into _simpleWeather()_ object
+
   $.simpleWeather({
     
     location: zipcode,
   
     success: function(weather) {
       
-      // Get & store temperature
+
       var temp = weather.temp;
-      // Get & store city
+
       var city = weather.city;
-      // Get & store currently
+
       var currently = weather.currently;
       var code = '<img src=' + weather.image + '>';
       var temp = weather.temp + '&deg;' + weather.units.temp;
-      
+
+      var wind = weather.wind.speed + ' mph'; 
+      var direction = '<i class="wi-windy"></i> ' + weather.wind.direction;
+
+      var low = '<i class="wi-down"></i> ' + weather.low + '&deg' + weather.units.temp;
+      var high = '<i class="wi-up"></i> ' + weather.high + '&deg' + weather.units.temp;
+
+      var sunrise = '<i class="wi-horizon-alt"></i> ' + weather.sunrise;
+      var sunset = '<i class="wi-horizon"></i> ' + weather.sunset; 
+
+      var link = weather.link;
+
       // Output to hooks in HTML
       $('.code').html(code);
       $('.temp').html(temp);
       $('.city').html(city);
       $('.currently').html(currently);
-      
+      $('.wind').html(wind);
+      $('.direction').html(direction);
+      $('.low').html(low);
+      $('.high').html(high);
+      $('.sunrise').html(sunrise);
+      $('.sunset').html(sunset);
+      $('link').html(link);
 
-      // See console for all properties of object
+
+
       console.log(weather);
     },
   
@@ -41,7 +59,6 @@ $(document).foundation();
   
   });
   
-  // 3. Reset input value
   $('#getWeather').val('');
   
 });
